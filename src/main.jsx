8c0167890798c942bbfb03168243ee078aa4657e
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-08-26 10:15:07 -0500

    app rendered

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,10 @@
 import "./index.css";
+import React from 'react';
+import App from './App';
+import ReactDOM from 'react-dom/client';
 
-// Render the App component to the root element
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
