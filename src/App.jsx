3fa90bb749aec31e52226af426f86939509070c5
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-08-26 11:13:07 -0500

    sections and with proper content are rendered. The dummy text is replaced with actual react-related info google shows first

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-import Header from './components/Header.jsx'
+import Header from './components/Header.jsx';
+import Section from './components/Section.jsx';
 
 function App() {
   return (
+    <>
     <Header />
+    <Section title='What is React?'>
+      <p>React is a JavaScript library that helps developers create graphical interfaces for web applications and native platforms.</p>
+    </Section>
+    <Section title='Benefits of React'>
+      <ul>
+        <li>Faster debugging and rendering</li>
+        <li>Reusability of code components</li>
+        <li>Readily available JavaScript Libraries</li>
+        <li>Use of Virtual Document Object Model</li>
+        <li> Easy to learn and use</li>
+      </ul>
+    </Section>
+    </>
   );
 }
 
